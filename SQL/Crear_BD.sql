afd568ecef22a796c692eaa3691d31e7e281793a
sebastianfernandocastro <sebastian.castro@alumnos.frgp.utn.edu.ar>
2024-11-03 00:42:30 +0100

    ...

--- SQL/Crear_BD.sql
+++ SQL/Crear_BD.sql
@@ -4,7 +4,7 @@ create database LAVADERO_DB
 
 use LAVADERO_DB
 
-create TABLE USUARIOS(
+create TABLE Cliente(
 ID Int identity(1,1) PRIMARY key,
 Nombre varchar(30) not null,
 Apellido varchar(30) not null,
@@ -13,4 +13,8 @@ MAIL varchar(40) not null unique,
 TELEFONO VARCHAR(20) not null,
 Usuario varchar(20) not null unique,
 Contrasenia varchar(20) not null,
+)
+
+Create TABLE Empleado(
+    
 )
