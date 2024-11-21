4b08a0db542322ccb3a3fb4769c13f6d7f7a8f01
Nico\Nicolás <nicojm197@gmail.com>
2024-11-21 17:02:54 -0300

    agregado filtro en rubro, se hizo que funcione el modificar, agregado checkbox mostrar inactivos

--- tp-cuatrimestral-equipo-17A/Rubros.aspx.designer.cs
+++ tp-cuatrimestral-equipo-17A/Rubros.aspx.designer.cs
@@ -15,6 +15,24 @@ namespace tp_cuatrimestral_equipo_17A
     {
 
         /// <summary>
+        /// Control lblFiltro.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblFiltro;
+
+        /// <summary>
+        /// Control txtFiltro.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtFiltro;
+
+        /// <summary>
         /// Control lblMensaje.
         /// </summary>
         /// <remarks>
@@ -22,6 +40,15 @@ namespace tp_cuatrimestral_equipo_17A
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label lblMensaje;
+
+        /// <summary>
+        /// Control cbxInactivos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox cbxInactivos;
 
         /// <summary>
         /// Control dgvRubros.
